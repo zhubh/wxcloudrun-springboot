58a6196de11f45e14fdef7609ee6532bb38aa4af
zhubh <zhubohaigsau@163.com>
2022-11-28 20:47:14 +0800

    Update UserInfoMapper.java

--- src/main/java/com/tencent/wxcloudrun/dao/UserInfoMapper.java
+++ src/main/java/com/tencent/wxcloudrun/dao/UserInfoMapper.java
@@ -1,15 +1,22 @@
 package com.tencent.wxcloudrun.dao;
 
+import com.tencent.wxcloudrun.model.BookMark;
 import com.tencent.wxcloudrun.model.UserInfo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface UserInfoMapper {
 
     UserInfo getUserInfo(@Param("userWxHm") String userWxHm);
 
-    void upsertUserInfo(UserInfo userInfo);
+    List<UserInfo> getUserinfoList(UserInfo userInfo);
 
-    void clearUserInfo(@Param("userWxHm") String userWxHm);
+    Integer updateUserInfo(UserInfo userInfo);
+
+    Integer upsertUserInfo(UserInfo userInfo);
+
+    Integer clearUserInfo(@Param("userWxHm") String userWxHm);
 }
