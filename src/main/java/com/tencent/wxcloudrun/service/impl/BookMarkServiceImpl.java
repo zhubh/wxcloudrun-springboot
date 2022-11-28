688a8b8e6cb0e2b7d01edcafefb2a9ad4b24a368
zhubh <zhubohaigsau@163.com>
2022-11-28 15:58:05 +0800

    Create BookMarkServiceImpl.java

---
+++ src/main/java/com/tencent/wxcloudrun/service/impl/BookMarkServiceImpl.java
@@ -0,0 +1,45 @@
+package com.tencent.wxcloudrun.service.impl;
+
+import com.tencent.wxcloudrun.dao.BookMarkMapper;
+import com.tencent.wxcloudrun.model.BookMark;
+import com.tencent.wxcloudrun.service.BookmarkService;
+import com.tencent.wxcloudrun.utils.RandomId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class BookMarkServiceImpl implements BookmarkService {
+
+    final BookMarkMapper bookMarkMapper;
+
+    public BookMarkServiceImpl(@Autowired BookMarkMapper bookMarkMapper) {
+        this.bookMarkMapper = bookMarkMapper;
+    }
+
+    @Override
+    public Optional<BookMark> getBookMark(String bookId) {
+        return Optional.ofNullable(bookMarkMapper.getBookMark(bookId));
+    }
+
+    @Override
+    public List<BookMark> getBookMarkByWxHM(String userWxHm) {
+        return bookMarkMapper.getBookMarkByWxHM(userWxHm);
+    }
+
+    @Override
+    public Integer upsertBookMark(BookMark bookMark) {
+          bookMark.setBookId(UUID.randomUUID().toString());
+        return bookMarkMapper.upsertBookMark(bookMark);
+    }
+
+    @Override
+    public Integer clearBookMark(String bookId) {
+        return bookMarkMapper.clearBookMark(bookId);
+    }
+
+
+}
