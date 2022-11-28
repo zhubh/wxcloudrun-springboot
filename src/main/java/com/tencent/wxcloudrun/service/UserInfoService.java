8bed1644c86be7644e7bb67766c7387605b0577b
zhubh <zhubohaigsau@163.com>
2022-11-28 20:45:49 +0800

    Update UserInfoService.java

--- src/main/java/com/tencent/wxcloudrun/service/UserInfoService.java
+++ src/main/java/com/tencent/wxcloudrun/service/UserInfoService.java
@@ -1,12 +1,18 @@
 package com.tencent.wxcloudrun.service;
 
+import com.tencent.wxcloudrun.model.BookMark;
 import com.tencent.wxcloudrun.model.UserInfo;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserInfoService {
 
     Optional<UserInfo> getUserInfo(String userWxHm);
+
+    List<UserInfo> getUserinfoList(UserInfo  userInfo);
+
+     Integer updateUserinfo(UserInfo userInfo);
 
     Integer upsertUserInfo(UserInfo userInfo);
 
