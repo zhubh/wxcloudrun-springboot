b9cdf494094c007199b180fd3731bb5de06263b3
zhubh <zhubohaigsau@163.com>
2022-11-29 11:12:06 +0800

    Create SwaggerConfig.java

---
+++ src/main/java/com/tencent/wxcloudrun/config/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package com.tencent.wxcloudrun.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Description:
+ * @Author: zhubohai
+ * @Date: 2022/11/29 0:16
+ * @Version: 1.0
+ **/
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+}
